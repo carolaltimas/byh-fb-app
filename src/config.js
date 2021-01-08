6ea5b250c69af50c0582e39c13f332e825767b17
carolaltimas <55726229+carolaltimas@users.noreply.github.com>
2021-01-07 21:14:07 -0500

    Updating stream

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 //export let API_BASE_URL = "https://us-central1-byh-server.cloudfunctions.net/app/api";
 
-export let API_BASE_URL = "https://us-central1-byh-app.cloudfunctions.net/app/api";
+//export let API_BASE_URL = "https://us-central1-byh-app.cloudfunctions.net/app/api";
 
-//export let API_BASE_URL = "http://localhost:5000/api";
+export let API_BASE_URL = "http://localhost:5000/api";
 
 
 export let pageConfig = [
